73984aa0ac99c87f006c73d7480635caae726b67
Milosz Szewczyk <milosz1092@gmail.com>
2019-10-09 16:08:38 +0200

    Upgrade all dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const dotenv = require('dotenv');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const { GenerateSW } = require('workbox-webpack-plugin');
 
