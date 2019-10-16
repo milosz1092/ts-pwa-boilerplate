19c2bca345170840099a72e1a4c5c3e61554499f
Milosz Szewczyk <milosz.szewczyk@avid.com>
2019-10-16 10:08:00 +0200

    Setup Jest

---
+++ src/StoreProvider.spec.ts
@@ -0,0 +1,5 @@
+describe('index', () => {
+    it('should be testable', () => {
+        expect(1).toBeTruthy();
+    });
+});
--- src/index.tsx
+++ src/index.tsx
@@ -9,48 +9,11 @@ import App from './components/App';
 
 const store = StoreProvider.getStore();
 
-const bootstrap = () => render(
+render(
     <Provider store={store}>
         <App history={history} />
     </Provider>,
     document.getElementById("app")
 );
 
-bootstrap();
 ServiceWorker.register();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function api<T>(url: string): Promise<T> {
-//     return fetch(url).then(response => {
-//         if (!response.ok) {
-//             throw new Error(response.statusText)
-//         }
-//         return response.json()
-//     });
-// }
-
-// api<any[]>('https://jsonplaceholder.typicode.com/users')
-//     .then((respBody) => {
-//         const data = respBody.map(entry => {
-//             return entry.name;
-//         });
-
-//         var mountNode = document.getElementById("app");
-//         ReactDOM.render(<App data={data} name="Miłosz" />, mountNode);
-//     })
-//     .catch(error => {
-//         /* show error message */
-//     })
